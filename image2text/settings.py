25d56e8a87dc8c9c3b9d76397fae4a279b528d3f
Afraim Zahangir <afraim.zahangir@gmail.com>
2025-11-13 19:02:25 +0600

    Set STATIC_ROOT for collectstatic in production

--- image2text/settings.py
+++ image2text/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Directory where `collectstatic` will collect static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional locations the staticfiles app will traverse
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
